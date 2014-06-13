bbef9ebfb79e8ef379a6154cde81642661d5b903
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-13 14:59:16 +0530

    square of x

---
+++ example/squareOfX.py
@@ -0,0 +1,4 @@
+#program to find square of x
+x = input("Enter x: ")      #take value of x
+sq = pow(int(x),2)          #convert x to int and find square using pow function
+print("Square: " + str(sq)) #print square by converting sq to string
