64ddd40da1377834e5c56d64a9dced211b4e56b5
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-13 14:59:30 +0530

    volume of box

---
+++ example/volumeOfBox.py
@@ -0,0 +1,6 @@
+#program to find volume of Box
+l = input("Enter length: ")
+b = input("Enter greadth: ")
+h = input("Enter height: ")
+volume = float(l)*float(b)*float(h)
+print("Volume: " + str(volume))
