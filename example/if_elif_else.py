16c4fc75cf94fc90c3ea6286145b8a967e812736
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-15 19:17:43 +0530

    if elif else

---
+++ example/if_elif_else.py
@@ -0,0 +1,15 @@
+#chained conditional
+#if elif else
+
+x = input("Enter x: ")
+
+if x > 0:
+    print x, "is positive"
+elif x < 0:
+    print x, "is negative"
+else:
+    print x, "is zero"
+
+
+if 0 < x < 10:
+    print x, "is in the range 1 to 9"
