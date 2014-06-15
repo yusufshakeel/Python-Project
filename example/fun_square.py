037b2a36b901f545ce121e79857ecd155c62f6f9
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-15 19:17:20 +0530

    function - square

---
+++ example/fun_square.py
@@ -0,0 +1,9 @@
+#square function
+
+#fuction definition
+def square(x):
+    return x*x
+
+x = input("Enter a number X: ")     #user input
+sq = square(x)                      #get square of x
+print "Square of x: " + str(sq)     #output
