288b0092641545529630e7f0e1a91b9217ef38e5
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-16 16:29:16 +0530

    module test - uses moduleCase.py

---
+++ example/moduleTest.py
@@ -0,0 +1,7 @@
+#this module will use the moduleCase.py file
+
+#import all the functions of the moduleCase
+from moduleCase import *
+
+ch = '123AbSskfkKJBKJknfkjds!@#$%^&*()fk'
+print toUpperCase(ch)
