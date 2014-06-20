da8bcbf11fe62a000191a42ff48ebeef6e1bea3b
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-20 11:31:49 +0530

    display name

---
+++ python3_turtle/name.py
@@ -0,0 +1,10 @@
+'''
+this is a python turtle program
+turtle is python's built-in graphic module for drawing
+'''
+
+#program to display name
+
+import turtle
+turtle.showturtle()
+turtle.write('My name is Yusuf Shakeel.')
