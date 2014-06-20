3b2e2bd8369551a3d73812514235d5ad98a833ae
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-06-20 11:31:43 +0530

    triangle

---
+++ python3_turtle/drawTriangle.py
@@ -0,0 +1,14 @@
+#turtle program to draw triangle
+
+import turtle
+turtle.showturtle()
+
+turtle.color('red')
+
+side = 200
+
+turtle.forward(side)
+turtle.left(120)
+turtle.forward(side)
+turtle.left(120)
+turtle.forward(side)
